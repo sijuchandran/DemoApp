1160b8ac78b7999d3bbde924e92c1550f8bad807
Siju Satheesachandran <ssatheesachandran@nat-w.local>
2018-11-11 22:05:33 +0000

    Initial Commit

---
+++ DemoFeedApp/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  DemoFeedApp
+//
+//  Created by Siju Satheesachandran on 11/11/2018.
+//  Copyright © 2018 Siju Satheesachandran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ DemoFeedApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DemoFeedApp
+//
+//  Created by Siju Satheesachandran on 11/11/2018.
+//  Copyright © 2018 Siju Satheesachandran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
