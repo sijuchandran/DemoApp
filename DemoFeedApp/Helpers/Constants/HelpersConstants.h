7b28457b5faa98b7fbacedd8e7dbbf81b9d29395
Siju Satheesachandran <ssatheesachandran@nat-w.local>
2018-11-13 11:36:56 +0000

    commit missing files

---
+++ DemoFeedApp/Helpers/Constants/HelpersConstants.h
@@ -0,0 +1,21 @@
+//
+//  HelpersConstants.h
+//  DemoFeedApp
+//
+//  Created by Siju Satheesachandran on 12/11/2018.
+//  Copyright © 2018 Siju Satheesachandran. All rights reserved.
+//
+
+#ifndef HelpersConstants_h
+#define HelpersConstants_h
+
+typedef enum
+{
+    kResponseOk = 0,
+    kResponseUnauthorised,
+    kResponseLockout,
+    kResponseError
+} kResponseType;
+
+
+#endif
